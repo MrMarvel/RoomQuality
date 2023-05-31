b793433076598ee6085b7cce2afaf87b87ede4c0
MrMarvel <seregakkk999@yandex.ru>
2023-05-31 22:57:31 +0300

    Added excel report writer

---
+++ app/src/main/java/ru/mrmarvel/hellofigma/util/Percentage.java
@@ -0,0 +1,13 @@
+package ru.mrmarvel.hellofigma.util;
+
+public enum Percentage {
+    TOILET,
+    WALL_FINISH,
+    WALL_ROUGH,
+    FLOOR_FINISH,
+    FLOOR_ROUGH,
+    RADIATOR,
+    CEILING,
+}
+
+
