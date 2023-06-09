7f9e7a20eb1f7cab5a68584ffa2599c431b1eb23
MrMarvel <seregakkk999@yandex.ru>
2023-06-09 23:11:03 +0300

    Add gradient bar

--- app/src/main/java/ru/mrmarvel/camoletapp/util/ExcelWriter.java
+++ app/src/main/java/ru/mrmarvel/camoletapp/util/ExcelWriter.java
@@ -13,21 +13,33 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Field;
 import java.util.HashMap;
 
 
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.ConditionalFormattingRule;
+import org.apache.poi.ss.usermodel.ConditionalFormattingThreshold;
+import org.apache.poi.ss.usermodel.DataBarFormatting;
+import org.apache.poi.ss.usermodel.ExtendedColor;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.SheetConditionalFormatting;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFConditionalFormattingRule;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFDataBarFormatting;
+import org.apache.xmlbeans.XmlObject;
 
 
 public class ExcelWriter {
 
     public XSSFWorkbook workbook;
     public XSSFWorkbook report;
+    public XSSFCellStyle style;
 
     //    private String nameTemplate = "Готовности по типу";
     public HashMap<Percentage, String> PercentageEnumToString = new HashMap<Percentage, String>(){{
@@ -67,15 +79,14 @@ public class ExcelWriter {
     }
 
     public void readWorkbook(Context context){
-//        BufferedReader reader = null;
         try{
-//            reader = new BufferedReader(
-//                    new InputStreamReader(context.getAssets().open("filename.txt")));
             InputStream file = context.getAssets().open("reportTemplate.xlsx");
             this.report = new XSSFWorkbook(file);
+
         } catch (Exception e){
             Log.d("READ ERROR", e.toString());
         }
+
     }
 
     public void fillReport(float[] data){
@@ -88,6 +99,34 @@ public class ExcelWriter {
         saveWorkbook("report.xlsx", this.report);
     }
 
+    public static void applyDataBars(SheetConditionalFormatting sheetCF, String region, ExtendedColor colorPos) {
+        CellRangeAddress[] regions = { CellRangeAddress.valueOf(region) };
+        ConditionalFormattingRule rule = sheetCF.createConditionalFormattingRule(colorPos);
+        DataBarFormatting dbf = rule.getDataBarFormatting();
+
+        dbf.getMinThreshold().setValue(0.0);
+        dbf.getMaxThreshold().setValue(1.0);
+
+        dbf.getMinThreshold().setRangeType(ConditionalFormattingThreshold.RangeType.NUMBER);
+        dbf.getMaxThreshold().setRangeType(ConditionalFormattingThreshold.RangeType.NUMBER);
+
+        dbf.setWidthMin(0); //cannot work for XSSFDataBarFormatting, see https://svn.apache.org/viewvc/poi/tags/REL_4_0_1/src/ooxml/java/org/apache/poi/xssf/usermodel/XSSFDataBarFormatting.java?view=markup#l57
+        dbf.setWidthMax(100); //cannot work for XSSFDataBarFormatting, see https://svn.apache.org/viewvc/poi/tags/REL_4_0_1/src/ooxml/java/org/apache/poi/xssf/usermodel/XSSFDataBarFormatting.java?view=markup#l64
+
+        if (dbf instanceof XSSFDataBarFormatting) {
+            try {
+                Field _databar = XSSFDataBarFormatting.class.getDeclaredField("_databar");
+                _databar.setAccessible(true);
+                org.openxmlformats.schemas.spreadsheetml.x2006.main.CTDataBar ctDataBar =
+                        (org.openxmlformats.schemas.spreadsheetml.x2006.main.CTDataBar) _databar.get(dbf);
+                ctDataBar.setMinLength(0);
+                ctDataBar.setMaxLength(100);
+            }
+            catch (Exception e){}
+        }
+        sheetCF.addConditionalFormatting(regions, rule);
+    }
+
     public void fillSheet(Percentage sheetName, int numFloor, int numSection, int maxFloor, float percent) {
         numFloor = maxFloor - numFloor;
         numFloor++;
@@ -96,11 +135,15 @@ public class ExcelWriter {
         String sheetN = PercentageEnumToString.get(sheetName);
         XSSFSheet currentSheet = this.workbook.getSheet(sheetN);
         Log.d("MYDEBUG", "START");
+        ExtendedColor colorPos = this.workbook.getCreationHelper().createExtendedColor();
+        colorPos.setARGBHex("FF638EC6");
         if (currentSheet == null) {
             currentSheet = this.workbook.createSheet(sheetN);
             Row sheetNameRow = currentSheet.createRow(0);
             Cell sheetNameCell = sheetNameRow.createCell(0);
             sheetNameCell.setCellValue(sheetN);
+            SheetConditionalFormatting sheetCF = currentSheet.getSheetConditionalFormatting();
+            applyDataBars(sheetCF, String.format("B2:W%s", maxFloor + 2), colorPos);
         }
         Log.d("MYDEBUG", "FIRST IF");
         // FILL CURRENT
@@ -113,7 +156,8 @@ public class ExcelWriter {
         floorCell.setCellValue(numFloor);
 
         Cell cell = row.createCell(numSection);
-        cell.setCellValue(percent);
+        cell.setCellValue(Math.random());
+
 
         Row sectionRow = currentSheet.getRow(maxFloor + 2);
         if (sectionRow == null){
@@ -139,46 +183,5 @@ public class ExcelWriter {
             e.printStackTrace();
         }
     }
-
-//    public void createSheet() {
-//        HSSFWorkbook workbook = new HSSFWorkbook();
-//
-//        //Create a blank sheet
-//        HSSFSheet sheet = workbook.createSheet("Employee Data");
-//
-//        //This data needs to be written (Object[])
-//        Map<String, Object[]> data = new TreeMap<String, Object[]>();
-//        data.put("1", new Object[]{"ID", "NAME", "LASTNAME"});
-//        data.put("2", new Object[]{1, "Amit", "Shukla"});
-//        data.put("3", new Object[]{2, "Lokesh", "Gupta"});
-//        data.put("4", new Object[]{3, "John", "Adwards"});
-//        data.put("5", new Object[]{4, "Brian", "Schultz"});
-//
-//        //Iterate over data and write to sheet
-//        Set<String> keyset = data.keySet();
-//        int rownum = 0;
-//        for (String key : keyset) {
-//            Row row = sheet.createRow(rownum++);
-//            Object[] objArr = data.get(key);
-//            int cellnum = 0;
-//            for (Object obj : objArr) {
-//                Cell cell = row.createCell(cellnum++);
-//                if (obj instanceof String)
-//                    cell.setCellValue((String) obj);
-//                else if (obj instanceof Integer)
-//                    cell.setCellValue((Integer) obj);
-//            }
-//        }
-//        try {
-//            File filePath = new File(Environment.getExternalStorageDirectory() + "/Demo.xls");
-//            //Write the workbook in file system
-//            FileOutputStream out = new FileOutputStream(filePath);
-//            workbook.write(out);
-//            out.close();
-//            System.out.println("howtodoinjava_demo.xlsx written successfully on disk.");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
 
